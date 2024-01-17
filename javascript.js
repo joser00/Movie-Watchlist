1985fcfc62fcf3a0165dc3ac9288459d4ba40a8c
Jose R <155120429+joser00@users.noreply.github.com>
2024-01-16 22:03:49 -0500

    Adding the last features of the project

--- javascript.js
+++ javascript.js
@@ -1,23 +1,30 @@
-let stringMovies = ""
+let stringMovies = "";
+const movieForm = document.getElementById("movie-form");
+const movieContainer = document.getElementById("movie-container")
+const inputMovie = document.getElementById("search-movie")
+let movieItem = []
 function getMovies() {
-  const movieForm = document.getElementById("movie-form");
   movieForm.addEventListener("submit", (e) => {
     e.preventDefault();
 
     const formData = new FormData(movieForm);
     let nameMovie = formData.get("searchMovie");
-  
+
     fetch(`http://www.omdbapi.com/?apikey=c6ecc1d3&s=${nameMovie}&type=movie`)
       .then((data) => data.json())
       .then((response) => {
         
         for (let movie of response.Search) {
-            fetch(`http://www.omdbapi.com/?apikey=c6ecc1d3&i=${movie.imdbID}&type=movie&plot=short`)
-            .then(response2 => response2.json())
-            .then(data => {
-              if(data.Poster==='N/A'){
-                console.log("En "+data.Tittle+" el poster no esta disponible")
-              }else {
+          fetch(
+            `http://www.omdbapi.com/?apikey=c6ecc1d3&i=${movie.imdbID}&type=movie&plot=short`
+          )
+            .then((response2) => response2.json())
+            .then((data) => {
+              if (data.Poster === "N/A") {
+                console.log(
+                 "El poster no esta disponible"
+                );
+              } else {
                 stringMovies += `
                 <div class="movie">
                 <img src=${data.Poster} alt="Image of the movie" /> 
@@ -30,7 +37,7 @@ function getMovies() {
                   <div class='options-movie'>
                   <p>${data.Runtime}</p>
                   <p>${data.Genre}</p>
-                  <p><i class="fa-solid fa-plus"></i> Watchlist</p>
+                  <p><i data-watchlist=${data.imdbID} class="fa-solid fa-plus" id="watchlist-btn"></i> Watchlist</p>
                   </div>
                   <p>
                   ${data.Plot}
@@ -40,15 +47,26 @@ function getMovies() {
                 <hr/>
                 `;
               }
-                document.getElementById("movie-container").innerHTML = stringMovies;
-                
-            })
-            stringMovies = ''
-            
+              movieContainer.innerHTML =stringMovies;
+            });
+          stringMovies = "";
         }
       });
-      document.getElementById('search-movie').value = ''
+    inputMovie.value = "";
   });
 }
 
 getMovies();
+
+function addMovie() {
+  
+  document.addEventListener('click', (e)=> {
+    if(e.target.dataset.watchlist){
+      movieItem.push(e.target.dataset.watchlist)
+      localStorage.setItem('movie',JSON.stringify(movieItem))
+    }
+  })
+  return movieItem
+}
+
+addMovie()
